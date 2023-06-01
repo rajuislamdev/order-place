edd9944ea226801c8e77ab10c7ec3e4f9e8d0e35
Raju Islam <rajuislamdev>
2023-06-01 19:21:35 +0600

    first commit

---
+++ lib/theme/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: const Color(0xFFF3F3F3),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: false,
+      titleTextStyle: TextStyle(color: Colors.black, fontSize: 22),
+      iconTheme: IconThemeData(
+        color: Color(0xFF113F67),
+      ),
+    ),
+    colorScheme: const ColorScheme.light().copyWith(
+      secondary: const Color(0xFF113F67),
+      primary: const Color(0xFF0059AA),
+      background: const Color(0xFFF3F3F3),
+      primaryContainer: const Color(0xFFFFFFFF),
+    ),
+    brightness: Brightness.light,
+    highlightColor: Colors.white,
+    hintColor: const Color(0xFF9E9E9E),
+    disabledColor: const Color(0xFF343A40),
+    textTheme: GoogleFonts.poppinsTextTheme(),
+  );
+}
